307d0c2138aad2972b8052322cbb52b10fd5787f
valentinod3m0n <valentino@demon.com>
2018-11-23 20:27:31 +0100

    Continued with the parser

--- src/parser.c
+++ src/parser.c
@@ -29,7 +29,8 @@ void load_and_parse(char* file_name){
 		perror("open");
 		exit(1);
 	}
-	
+
+	//Get labels and variables	
 	while (fgets(buffer, BUFFER_MAX, f)){
 		//The first call to strtok has the string, the following will have just NULL.
 		//The condition will be token != NULL
@@ -55,6 +56,7 @@ void load_and_parse(char* file_name){
 			
 			//Label
 			if (token[strlen(token) - 1] == ':'){
+				//TODO
 				//Add the label
 				//Remember that we can have a label followed by a instruction.
 				continue;
@@ -62,10 +64,12 @@ void load_and_parse(char* file_name){
 
 			//Variable
 			if (token[0] == '.'){
+				//TODO
 				//Add variable
 				//Need to keep track of .data size.
 				break;
 			} else {
+				//TODO
 				//Instruction
 				//Need to keep track of .text size.
 				break;
@@ -75,6 +79,43 @@ void load_and_parse(char* file_name){
 		//We don't know what will remain in the buffer.
 		memset(0, buffer, BUFFER_MAX);
 	}
+
+	//Now we analyze again to get the variables
+	
+	lseek(f, 0, SEEK_SET);
+	while (fgets(buffer, BUFFER_MAX, f)){
+		for (token = strtok(buffer, delim); token; token = strtok(NULL, delim)){
+			//Comments
+			if (token[0] == '#')
+				break;
+			
+			//Segments
+			if (strcmp(token, ".text") == 0){
+				current_segment = TEXT;
+				break;
+			}
+
+			if (strcmp(token, ".data") == 0){
+				current_segment = DATA;
+				break;
+			}
+
+			//Extern
+			if (strcmp(token, ".global") == 0)
+				break;
+			//We previously added the variables so no reason to go through .data again
+			if(current_segment == DATA)
+				break;
+			//Label
+			if (token[strlen(token) - 1] == ':')
+				break;
+			//TODO
+			//Instruction
+			//Need to keep track of .text size.
+		}
+	
+	}
+
 }
 
 struct instruction* parse_instruction(char* line){
